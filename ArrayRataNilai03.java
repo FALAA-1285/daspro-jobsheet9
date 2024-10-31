ee95c3573991d099f47124910eb10271173630ba
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-10-31 11:55:19 +0700

    pertanyaan dari percobaan 3

--- ArrayRataNilai03.java
+++ ArrayRataNilai03.java
@@ -5,18 +5,26 @@ public class ArrayRataNilai03{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        int[] nilaiMhs= new int[10];
         double total=0, rata2;
+        System.out.print("masukkan jumlah mahasiswa: ");
+        int p=sc.nextInt();
+        int[] nilaiMhs= new int[p];
+        
 
         for (int i=0; i<nilaiMhs.length; i++ ){
             System.out.print("masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i]=sc.nextInt();
-        }
-        for(int i=0; i<nilaiMhs.length; i++){
+        }for(int i=0; i<nilaiMhs.length; i++){
             total+=nilaiMhs[i];
         }
-
         rata2=total/nilaiMhs.length;
-        System.out.println("rata rata nilai = "+rata2);
+        System.out.println("rata rata nilai = "+rata2);        
+        for (int i=0; i<nilaiMhs.length; i++){
+            if(nilaiMhs[i]>70){
+                System.out.println("mahasiswa ke-"+(i+1)+" lulus");
+            }else{
+                System.out.println("mahasiswa ke-"+(i+1)+" tidak lulus");
+            }
+        }
     }
 }
